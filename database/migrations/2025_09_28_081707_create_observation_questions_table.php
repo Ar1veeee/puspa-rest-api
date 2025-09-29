e39033958c2f43a0a45b82005b5d6015b6b74acd
Ar1veeee <aliefarifin99@gmail.com>
2025-09-29 11:19:24 +0700

    feat: add observation question and answer table

---
+++ database/migrations/2025_09_28_081707_create_observation_questions_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('observation_questions', function (Blueprint $table) {
+            $table->id();
+            $table->string('question_code', 6)->unique();
+            $table->enum('age_category', ['balita', 'anak-anak', 'remaja', 'lainya']);
+            $table->integer('question_number');
+            $table->text('question_text');
+            $table->integer('score');
+            $table->boolean('is_active')->default(true);
+            $table->timestamps();
+
+            $table->unique(['age_category', 'question_number']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('observation_questions');
+    }
+};
