4fedce96e8362c59abcf714644a3bea27174ca3e
Ar1veeee <aliefarifin99@gmail.com>
2025-09-29 17:05:06 +0700

    feat: use custom format data on child age and birth date

--- app/Http/Resources/ChildrenResource.php
+++ app/Http/Resources/ChildrenResource.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Resources;
 
-use App\Models\Child;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
@@ -15,18 +14,11 @@ class ChildrenResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        $age = null;
-        if ($this->child && $this->child->child_birth_date) {
-            $ageInfo = Child::calculateAgeAndCategory($this->child->child_birth_date);
-            $age = $ageInfo['age'];
-        }
-
         return [
             "id" => $this->id,
             'child_name' => $this->child_name,
             'child_birth_date' => $this->child_birth_date->format('d F Y'),
             'child_age' => $this->child_birth_date->diff(now())->format('%y Tahun %m Bulan'),
-            // 'child_age' => $age,
             'child_gender' => $this->child_gender,
             'child_school' => $this->child_school,
         ];
